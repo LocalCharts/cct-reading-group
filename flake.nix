7aca5221256c81e5a0adddfa783a6497f27dad8a
akielbowicz <augusto.kiel@gmail.com>
2024-05-06 21:22:09 -0300

    restore flake.nix and add open statements to test

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "CCT OCaml dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+        pkgs.ocaml
+        pkgs.ocamlformat
+        pkgs.opam
+        pkgs.dune_3
+        pkgs.ocamlPackages.findlib
+        pkgs.ocamlPackages.utop
+        pkgs.ocamlPackages.ocaml-lsp
+        pkgs.ocamlPackages.owl
+        ];
+      };
+      env = {
+        CAML_LD_LIBRARY_PATH = "${pkgs.ocaml}/lib/ocaml/stublibs:${pkgs.ocaml}/lib/ocaml:${pkgs.ocamlPackages.utop}/lib/ocaml";
+      };
+    };
+}
